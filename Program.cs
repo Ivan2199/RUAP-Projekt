36418642275789d51c7229928d32c2255b19c66d
unknown <jankovic2107@gmail.com>
2022-10-26 16:14:35 +0200

    VS-datoteka

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pozdrav iz aplikacije od [Ivan Jankovic]“");
+            Console.WriteLine("01656532");
         }
     }
 }
